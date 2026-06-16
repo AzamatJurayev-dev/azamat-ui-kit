54f5bba496acd3982fe8cfdb2008091aa91cd6d4
Azamat Jurayev <jurayevazamat007@gmail.com>
2026-06-16 10:27:56 +0500

    feat: export phase 1 components

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,16 @@
-import "./index.css";
+import "./index.css"
 
+export * from "./components/ui/button"
+export * from "./components/ui/input"
+export * from "./components/ui/dialog"
+export * from "./components/ui/dropdown-menu"
+export * from "./components/ui/select"
+export * from "./components/ui/table"
+export * from "./components/ui/badge"
+export * from "./components/ui/card"
 
-export * from "./components/ui/button";
-export * from "./components/ui/input";
-export * from "./components/ui/dialog";
-export * from "./components/ui/dropdown-menu";
-export * from "./components/ui/select";
-export * from "./components/ui/table";
-export * from "./components/ui/badge";
-export * from "./components/ui/card";
-
-
-export * from "./lib/utils";
+export * from "./components/overlay"
+export * from "./components/navigation"
+export * from "./components/inputs"
+export * from "./hooks"
+export * from "./lib/utils"
