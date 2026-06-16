ecd8adbba0c4e33d1347114910e8cf5eb99630ee
Azamat Jurayev <jurayevazamat007@gmail.com>
2026-06-16 11:43:42 +0500

    feat: wire cli list and add options

--- cli/index.ts
+++ cli/index.ts
@@ -1,23 +1,32 @@
 import { Command } from "commander";
 import { initCommand } from "./commands/init";
 import { addCommand } from "./commands/add";
+import { listCommand } from "./commands/list";
 
 const program = new Command();
 
 program
-    .name("azamat-ui-kit")
-    .description("Azamat UI Kit CLI")
-    .version("0.0.1");
+  .name("azamat-ui-kit")
+  .description("Azamat UI Kit CLI")
+  .version("0.0.1");
 
 program
-    .command("init")
-    .description("Initialize Azamat UI Kit in your project")
-    .action(initCommand);
+  .command("init")
+  .description("Initialize Azamat UI Kit in your project")
+  .action(initCommand);
 
 program
-    .command("add")
-    .description("Add components to your project")
-    .argument("[components...]", "components to add")
-    .action(addCommand);
+  .command("list")
+  .description("List available registry components")
+  .action(listCommand);
+
+program
+  .command("add")
+  .description("Add components to your project")
+  .argument("[components...]", "components to add")
+  .option("-o, --overwrite", "overwrite existing files")
+  .option("--dry-run", "show files without writing")
+  .option("--skip-install", "do not install package dependencies")
+  .action(addCommand);
 
 program.parse();
