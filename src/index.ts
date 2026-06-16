bbaf9174679005f9e5eec33e5fc8314b03de4a43
Azamat Jurayev <jurayevazamat007@gmail.com>
2026-06-16 10:47:30 +0500

    feat: export textarea primitive

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import "./index.css"
 
 export * from "./components/ui/button"
 export * from "./components/ui/input"
+export * from "./components/ui/textarea"
 export * from "./components/ui/dialog"
 export * from "./components/ui/dropdown-menu"
 export * from "./components/ui/popover"
