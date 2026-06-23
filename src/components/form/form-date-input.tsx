5d9950c580ee3a0a214a41e7c6aaea834001f85b
Azamat Jurayev <jurayevazamat007@gmail.com>
2026-06-23 23:17:00 +0500

    Update form date wrapper

--- src/components/form/form-date-input.tsx
+++ src/components/form/form-date-input.tsx
@@ -1,19 +1,16 @@
-import {
-  FormInput,
-  type FormInputDateVariantProps as BaseFormDateInputProps,
-} from "@/components/form/form-input"
+import { FormAppInput, type FormAppInputProps } from "@/components/form/form-app-input"
 import type { FieldPath, FieldValues } from "react-hook-form"
 
 export type FormDateInputProps<
   TFieldValues extends FieldValues = FieldValues,
   TName extends FieldPath<TFieldValues> = FieldPath<TFieldValues>,
-> = Omit<BaseFormDateInputProps<TFieldValues, TName>, "kind">
+> = Omit<FormAppInputProps<TFieldValues, TName>, "kind">
 
 function FormDateInput<
   TFieldValues extends FieldValues = FieldValues,
   TName extends FieldPath<TFieldValues> = FieldPath<TFieldValues>,
 >(props: FormDateInputProps<TFieldValues, TName>) {
-  return <FormInput {...props} kind="date" />
+  return <FormAppInput {...props} kind="date" />
 }
 
 export { FormDateInput }
