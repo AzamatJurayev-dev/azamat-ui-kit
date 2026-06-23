8b70d4b9e5e1fdd4a7f2f875f0b8a23e5b7c2814
Azamat Jurayev <jurayevazamat007@gmail.com>
2026-06-23 23:16:06 +0500

    Delegate form number input to universal wrapper

--- src/components/form/form-number-input.tsx
+++ src/components/form/form-number-input.tsx
@@ -1,19 +1,16 @@
-import {
-  FormInput,
-  type FormInputNumberVariantProps as BaseFormNumberInputProps,
-} from "@/components/form/form-input"
+import { FormAppInput, type FormAppInputProps } from "@/components/form/form-app-input"
 import type { FieldPath, FieldValues } from "react-hook-form"
 
 export type FormNumberInputProps<
   TFieldValues extends FieldValues = FieldValues,
   TName extends FieldPath<TFieldValues> = FieldPath<TFieldValues>,
-> = Omit<BaseFormNumberInputProps<TFieldValues, TName>, "kind">
+> = Omit<FormAppInputProps<TFieldValues, TName>, "kind">
 
 function FormNumberInput<
   TFieldValues extends FieldValues = FieldValues,
   TName extends FieldPath<TFieldValues> = FieldPath<TFieldValues>,
 >(props: FormNumberInputProps<TFieldValues, TName>) {
-  return <FormInput {...props} kind="number" />
+  return <FormAppInput {...props} kind="number" />
 }
 
 export { FormNumberInput }
